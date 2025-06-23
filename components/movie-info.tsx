fb495c87d66d74062669b7448924c59f5db783ce
lru0409 <lru040409@gmail.com>
2025-06-23 10:37:37 +0900

    feat: complete MovieDetail page

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,12 +1,26 @@
 import { API_URL } from '../app/(home)/page';
+import styles from '../styles/movie-info.module.css';
 
-async function getMovie(id: string) {
+export async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
   const json = await response.json();
   return json;
 }
 
 export default async function MovieInfo({ id }: { id: string }) {
-  const videos = await getMovie(id);
-  return <div>{JSON.stringify(videos)}</div>;
+  const movie = await getMovie(id);
+
+  return (
+    <div className={styles.container}>
+      <img src={movie.poster_path} className={styles.poster} />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={'_blank'}>
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 }
